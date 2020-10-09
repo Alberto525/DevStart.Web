407bb344dc8b8f190d70887bc80af2319b4ca68f
Alberto <albertogonzalesa95@gmail.com>
2020-10-09 01:33:53 -0500

    Mantenimiento Empresa

---
+++ Web.Marcacion/Models/T_Empresa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web.Marcacion.Models
+{
+    public class T_Empresa
+    {
+        [Key]
+        public int ID_Empresa { get; set; }
+        public string RazonSocial { get; set; }
+        public string RUC { get; set; }
+        public string Grupo { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public int UsuarioCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public int UsuarioModificacion { get; set; }
+        public bool Estado { get; set; }
+
+
+
+    }
+}
