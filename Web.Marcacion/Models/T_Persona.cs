09734b98259e75485e1e296d6c8981dfb73e6ef2
Marcos Mendoza <mirio.marcos@gmail.com>
2020-10-26 19:53:20 -0500

    Albertotototo

--- Web.Marcacion/Models/T_Persona.cs
+++ Web.Marcacion/Models/T_Persona.cs
@@ -19,14 +19,14 @@ namespace Web.Marcacion.Models
         public string Correo { get; set; }
         public string Movil { get; set; }
         public string Direccion { get; set; }
-        public DateTime? FechaNacimiento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
         public string Cargo { get; set; }
         public DateTime FechaCreacion { get; set; }
         public int UsuarioCreacion { get; set; }
 
-        public DateTime? FechaModificacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
         public int UsuarioModificacion { get; set; }
         public bool Estado { get; set; }
-        public int? ID_Cargo { get; set; }
+        public int ID_Cargo { get; set; }
     }
 }
